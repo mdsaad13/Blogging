51e928e612cfe8a7185cf821eaed7cd28afc1f6f
Mohammed Saad <saad.surana@gmail.com>
2020-02-21 00:55:33 +0530

    Add project files.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Blogging
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css/main").Include(
+                      "~/Content/adminlte.css",
+                      "~/Content/OverlayScrollbars.css",
+                      "~/Content/style.css,"));
+
+            bundles.Add(new ScriptBundle("~/js/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/js/main").Include(
+                      "~/Scripts/jquery-3.4.1.min.js",
+                      "~/Scripts/bootstrap.bundle.min.js",
+                      "~/Scripts/adminlte.js",
+                      "~/Scripts/overlayScrollbars.js"));
+        }
+    }
+}
---
+++ Models/AccountModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blogging.Models
+{
+    public class AccountModel
+    {
+        public int ID { get; set; }
+
+
+        [Required(ErrorMessage = "What's your name?")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Enter a valid name")]
+        public string Name { get; set; }
+        
+
+        [Required(ErrorMessage = "Kindly enter your email")]
+        [EmailAddress(ErrorMessage = "Enter valid email")]
+        public string Email { get; set; }
+        
+
+        [Required(ErrorMessage = "You'll use this when you log in and if you ever need to reset your password.")]
+        [Display(Name = "Mobile Number")]
+        [RegularExpression("^([6-9]{1})([0-9]{9})$", ErrorMessage = "Enter a valid mobile number")]
+        public string Mobile { get; set; }
+        
+        
+        [Required(ErrorMessage = "You'll use this when you log in and if you ever need to reset your password.")]
+        [Display(Name = "User Name")]
+        [RegularExpression("^([a-zA-Z0-9_.]{5,30})$", ErrorMessage = "Username should be atleast of 5 characters and can only include _ and . special characters")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Please choose a gender")]
+        public string Gender { get; set; }
+        
+
+        [Required(ErrorMessage = "It looks like you've entered the wrong info. Please make sure that you use your real date of birth")]
+        [Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+
+
+        [Required(ErrorMessage = "Kindly enter your password")]
+        [Display(Name = "Password")]
+        [StringLength(16, MinimumLength = 6, ErrorMessage = "Enter a valid password")]
+        [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&_+-=]{6,16}$", ErrorMessage = "Enter a valid password")]
+        public string Password { get; set; }
+
+
+        [Display(Name = "Re-Enter Password")]
+        [Compare("Password", ErrorMessage = "Entered Password did not match")]
+        public string ConfirmPass { get; set; }
+
+        [Display(Name = "Profile picture")]
+        public string ImgUrl { get; set; }
+
+
+        [RegularExpression(@"([a-zA-Z0-9\s_\\.\-:])+(.png|.jpg|.gif|.jpeg)$", ErrorMessage = "It dosen't look like an image")]
+        public HttpPostedFileBase Image { get; set; }
+    }
+}
